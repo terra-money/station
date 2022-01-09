d737ba6f138a268edab78ea4f4bf73e2550ebe79
sim <sim@terra.money>
2022-01-09 18:45:46 +0900

    Update timestamp component every seconds

--- src/components/display/ToNow.tsx
+++ src/components/display/ToNow.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
 import { toNow } from "utils/date"
+import useInterval from "utils/hooks/useInterval"
 import Tooltip from "./Tooltip"
 
-const ToNow = ({ children: date }: { children: Date }) => {
+interface Props {
+  children: Date
+  update?: boolean
+}
+
+const ToNow = ({ children: date, update }: Props) => {
+  const [, setKey] = useState(0)
+  useInterval(
+    () => {
+      setKey((key) => key + 1)
+    },
+    update ? 1000 : null
+  )
+
   return (
-    <Tooltip content={date.toString()}>
+    <Tooltip content={date.toLocaleString()}>
       <span>{toNow(date)}</span>
     </Tooltip>
   )
