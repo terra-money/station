4993847820bf67136ed53943a1659698e7d42250
sim <sim@terra.money>
2022-01-20 17:31:33 +0900

    refactor: Extract selector component for preconfigured accounts

---
+++ src/auth/modules/select/SelectPreconfigured.tsx
@@ -0,0 +1,42 @@
+import { useTranslation } from "react-i18next"
+import usePreconfigured from "auth/hooks/usePreconfigured"
+import { useNetwork } from "data/wallet"
+import { Select } from "components/form"
+import useAuth from "../../hooks/useAuth"
+
+const SelectPreconfigured = () => {
+  const { t } = useTranslation()
+  const { connectedWallet, connectPreconfigured } = useAuth()
+  const preconfigured = usePreconfigured()
+  const { preconfigure } = useNetwork()
+
+  if (!preconfigure) return null
+
+  const selected = preconfigured.find(
+    ({ name }) => name === connectedWallet?.name
+  )
+
+  return (
+    <Select
+      value={selected?.name ?? ""}
+      onChange={(e) => {
+        const wallet = preconfigured.find(({ name }) => name === e.target.value)
+        if (wallet) connectPreconfigured(wallet)
+      }}
+    >
+      <option value="" disabled>
+        {t("Preconfigured wallets...")}
+      </option>
+
+      {preconfigured.map(({ name }) => {
+        return (
+          <option value={name} key={name}>
+            {name}
+          </option>
+        )
+      })}
+    </Select>
+  )
+}
+
+export default SelectPreconfigured
--- src/auth/modules/select/SwitchWallet.tsx
+++ src/auth/modules/select/SwitchWallet.tsx
@@ -1,46 +1,18 @@
-import { useTranslation } from "react-i18next"
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
 import { truncate } from "@terra.kitchen/utils"
-import usePreconfigured from "auth/hooks/usePreconfigured"
 import classNames from "classnames/bind"
-import { useNetwork } from "data/wallet"
-import { Select } from "components/form"
 import { Flex } from "components/layout"
 import AuthButton from "../../components/AuthButton"
 import MultisigBadge from "../../components/MultisigBadge"
 import useAuth from "../../hooks/useAuth"
 import is from "../../scripts/is"
+import SelectPreconfigured from "./SelectPreconfigured"
 import styles from "./SwitchWallet.module.scss"
 
 const cx = classNames.bind(styles)
 
 const SwitchWallet = () => {
-  const { t } = useTranslation()
-  const { connectedWallet, wallets, connect, connectPreconfigured } = useAuth()
-  const preconfigured = usePreconfigured()
-  const { preconfigure } = useNetwork()
-
-  const preconfiguredWallets = preconfigure && (
-    <Select
-      value={connectedWallet?.name ?? ""}
-      onChange={(e) => {
-        const wallet = preconfigured.find(({ name }) => name === e.target.value)
-        if (wallet) connectPreconfigured(wallet)
-      }}
-    >
-      <option value="" disabled>
-        {t("Preconfigured wallets...")}
-      </option>
-
-      {preconfigured.map(({ name }) => {
-        return (
-          <option value={name} key={name}>
-            {name}
-          </option>
-        )
-      })}
-    </Select>
-  )
+  const { connectedWallet, wallets, connect } = useAuth()
 
   const localWallets = !!wallets.length && (
     <ul className={styles.list}>
@@ -79,7 +51,7 @@ const SwitchWallet = () => {
 
   return (
     <>
-      {preconfiguredWallets}
+      <SelectPreconfigured />
       {localWallets}
     </>
   )
