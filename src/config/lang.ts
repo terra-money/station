536b67b04c861e878432ec59b1ae7e577326a59e
BlockJayn <99530800+BlockJayn@users.noreply.github.com>
2022-02-19 16:47:02 +0100

    Add german language (DE) entries
    As requested, to make german language available "de" has been added in line 8 and 28.
    https://github.com/terra-money/station/pull/32#pullrequestreview-887891679

--- src/config/lang.ts
+++ src/config/lang.ts
@@ -5,6 +5,7 @@ import { Dictionary } from "ramda"
 import { debug } from "utils/env"
 
 import cs from "locales/cs.json"
+import de from "locales/de.json"
 import es from "locales/es.json"
 import fr from "locales/fr.json"
 import it from "locales/it.json"
@@ -24,6 +25,7 @@ const flatten = (obj: object, initial = {}): Dictionary<string> => {
 
 export const Languages = {
   cs: { value: "cs", label: "Čeština", translation: flatten(cs) },
+  de: { value: "de", label: "Deutsch", translation: flatten(de) },
   en: { value: "en", label: "English", translation: {} },
   es: { value: "es", label: "Español", translation: flatten(es) },
   fr: { value: "fr", label: "Français", translation: flatten(fr) },
