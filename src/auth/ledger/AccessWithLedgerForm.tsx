7ec3dca1ad98cb841307ede7925b819d0ba4f773
Alessandro Candeago <alessandro@candeago.dev>
2022-02-25 14:54:32 +0100

    Connect Ledger with bluetooth

--- src/auth/ledger/AccessWithLedgerForm.tsx
+++ src/auth/ledger/AccessWithLedgerForm.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react"
-import { useTranslation } from "react-i18next"
-import { useNavigate } from "react-router-dom"
-import { useForm } from "react-hook-form"
-import UsbIcon from "@mui/icons-material/Usb"
-import { LedgerKey } from "@terra-money/ledger-terra-js"
-import { Form, FormError, FormItem, FormWarning } from "components/form"
-import { Input, Submit } from "components/form"
-import validate from "../scripts/validate"
-import useAuth from "../hooks/useAuth"
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import UsbIcon from '@mui/icons-material/Usb'
+import { LedgerKey } from '@terra-money/ledger-terra-js'
+import BluetoothTransport from '@ledgerhq/hw-transport-web-ble'
+import { Checkbox, Form, FormError, FormItem, FormWarning } from 'components/form'
+import { Input, Submit } from 'components/form'
+import validate from '../scripts/validate'
+import useAuth from '../hooks/useAuth'
 
 interface Values {
   index: number
@@ -18,11 +19,12 @@ const AccessWithLedgerForm = () => {
   const navigate = useNavigate()
   const { connectLedger } = useAuth()
   const [isLoading, setIsLoading] = useState(false)
+  const [bluetooth, setBluetooth] = useState(false)
   const [error, setError] = useState<Error>()
 
   /* form */
   const form = useForm<Values>({
-    mode: "onChange",
+    mode: 'onChange',
     defaultValues: { index: 0 },
   })
 
@@ -35,9 +37,12 @@ const AccessWithLedgerForm = () => {
     setError(undefined)
 
     try {
-      const { accAddress } = await LedgerKey.create(undefined, index)
-      connectLedger(accAddress, index)
-      navigate("/wallet", { replace: true })
+      const { accAddress } = await LedgerKey.create(
+        bluetooth ? await BluetoothTransport.create(120_000) : undefined,
+        index,
+      )
+      connectLedger(accAddress, index, bluetooth)
+      navigate('/wallet', { replace: true })
     } catch (error) {
       setError(error as Error)
     } finally {
@@ -49,25 +54,25 @@ const AccessWithLedgerForm = () => {
     <Form onSubmit={handleSubmit(submit)}>
       <section className="center">
         <UsbIcon style={{ fontSize: 56 }} />
-        <p>{t("Plug in a Ledger device")}</p>
+        <p>{t('Plug in a Ledger device')}</p>
       </section>
 
-      <FormItem /* do not translate this */
-        label="Index"
-        error={errors.index?.message}
-      >
+      <FormItem /* do not translate this */ label="Index" error={errors.index?.message}>
         <Input
-          {...register("index", {
+          {...register('index', {
             valueAsNumber: true,
             validate: validate.index,
           })}
         />
-        {index !== 0 && <FormWarning>{t("Default index is 0")}</FormWarning>}
+        <Checkbox checked={bluetooth} onChange={() => setBluetooth(!bluetooth)}>
+          Use Bluetooth (only for Nano X)
+        </Checkbox>
+        {index !== 0 && <FormWarning>{t('Default index is 0')}</FormWarning>}
       </FormItem>
 
       {error && <FormError>{error.message}</FormError>}
 
-      <Submit submitting={isLoading}>{t("Connect")}</Submit>
+      <Submit submitting={isLoading}>{t('Connect')}</Submit>
     </Form>
   )
 }
