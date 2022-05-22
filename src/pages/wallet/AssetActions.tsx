f251b9f4bf29a81b6b75718a4929892518a9b748
sim <sim@terra.money>
2022-05-22 17:54:35 +0900

    enable swap on classic network

--- src/pages/wallet/AssetActions.tsx
+++ src/pages/wallet/AssetActions.tsx
@@ -1,7 +1,12 @@
 import { useTranslation } from "react-i18next"
+import { flatten, uniq } from "ramda"
 import ShortcutOutlinedIcon from "@mui/icons-material/ShortcutOutlined"
+import RestartAltIcon from "@mui/icons-material/RestartAlt"
+import { isDenomTerraNative } from "@terra.kitchen/utils"
 import { has } from "utils/num"
+import { useIsClassic } from "data/query"
 import { useIsWalletEmpty } from "data/queries/bank"
+import { useCW20Pairs } from "data/Terra/TerraAssets"
 import { InternalLink } from "components/general"
 import { ExtraActions } from "components/layout"
 import { Props } from "./Asset"
@@ -9,6 +14,8 @@ import { Props } from "./Asset"
 const AssetActions = ({ token, symbol, balance }: Props) => {
   const { t } = useTranslation()
   const isWalletEmpty = useIsWalletEmpty()
+  const isClassic = useIsClassic()
+  const getIsSwappableToken = useGetIsSwappableToken()
 
   return (
     <ExtraActions>
@@ -19,8 +26,33 @@ const AssetActions = ({ token, symbol, balance }: Props) => {
       >
         {t("Send")}
       </InternalLink>
+
+      {isClassic && (
+        <InternalLink
+          icon={<RestartAltIcon style={{ fontSize: 18 }} />}
+          to="/swap"
+          state={token}
+          disabled={
+            isWalletEmpty || !has(balance) || !getIsSwappableToken(token)
+          }
+        >
+          {t("Swap")}
+        </InternalLink>
+      )}
     </ExtraActions>
   )
 }
 
 export default AssetActions
+
+/* helpers */
+const useGetIsSwappableToken = () => {
+  const { data: pairs } = useCW20Pairs()
+
+  return (token: TerraAddress) => {
+    if (isDenomTerraNative(token)) return true
+    if (!pairs) return false
+    const terraswapAvailableList = uniq(flatten(Object.values(pairs)))
+    return terraswapAvailableList.find(({ assets }) => assets.includes(token))
+  }
+}
