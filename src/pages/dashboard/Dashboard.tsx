ca25d91d27a8876d9ddb0b23a99026b2112323d2
sim <sim@terra.money>
2022-05-26 17:42:19 +0900

    Layout dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next"
+import classNames from "classnames/bind"
 import { useIsClassic } from "data/query"
 import { Col, Page } from "components/layout"
 import LunaPrice from "./LunaPrice"
@@ -8,6 +9,8 @@ import StakingRatio from "./StakingRatio"
 import Charts from "./Charts"
 import styles from "./Dashboard.module.scss"
 
+const cx = classNames.bind(styles)
+
 const Dashboard = () => {
   const { t } = useTranslation()
   const isClassic = useIsClassic()
@@ -15,7 +18,7 @@ const Dashboard = () => {
   return (
     <Page title={t("Dashboard")}>
       <Col>
-        <header className={styles.header}>
+        <header className={cx(styles.header, { trisect: !isClassic })}>
           {isClassic && <LunaPrice />}
           <Issuance />
           <CommunityPool />
