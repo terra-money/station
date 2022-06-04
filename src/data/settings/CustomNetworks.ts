80b92049b55047271410651e43bc37810e92b4c5
sim <sim@terra.money>
2022-06-04 16:17:11 +0900

    Deprecate Anchor Earn from mainnet

--- src/data/settings/CustomNetworks.ts
+++ src/data/settings/CustomNetworks.ts
@@ -13,7 +13,7 @@ export const useCustomNetworks = () => {
 
   const validateName = (name: string) => {
     return (
-      !["mainnet", "testnet", "localterra"].includes(name) &&
+      !["mainnet", "classic", "testnet", "localterra"].includes(name) &&
       !customNetworks.some((network) => network.name === name)
     )
   }
