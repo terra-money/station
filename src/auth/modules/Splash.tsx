6de01827d0f0b76fc48ab5b0dc02961e1a13ce75
Samuel Kim <samuel@kernellabs.co>
2022-07-13 16:28:09 +0900

    Update Splash styles with theme color

--- src/auth/modules/Splash.tsx
+++ src/auth/modules/Splash.tsx
@@ -1,15 +1,19 @@
 import { useTranslation } from "react-i18next"
-import { useThemeAnimation } from "data/settings/Theme"
+import { useTheme, useThemeAnimation } from "data/settings/Theme"
 import { FlexColumn } from "components/layout"
 import { sandbox } from "../scripts/env"
 import styles from "./Splash.module.scss"
+import classNames from "classnames/bind"
+
+const cx = classNames.bind(styles)
 
 const Splash = () => {
   const { t } = useTranslation()
   const animation = useThemeAnimation()
+  const { name } = useTheme()
 
   return !sandbox ? null : (
-    <FlexColumn gap={20} className={styles.screen}>
+    <FlexColumn gap={20} className={cx(styles.screen, { [name]: true })}>
       <img src={animation} alt="" width={80} height={80} />
       <h1>{t("Initializing app...")}</h1>
     </FlexColumn>
