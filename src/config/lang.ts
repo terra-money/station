c411ecd12a4b48e9cd1cda2583ec96084bc6ffc1
Samuel Kim <samuel@kernellabs.co>
2022-07-13 11:29:20 +0900

    fix init lng en

--- src/config/lang.ts
+++ src/config/lang.ts
@@ -42,7 +42,7 @@ const whitelist = ["cs", "de", "en", "es", "fr", "it", "pl", "pt", "ru", "zh"]
 
 i18n.use(LanguageDetector).use(initReactI18next).init({
   resources: Languages,
-  fallbackLng: "en",
+  lng: "en",
   supportedLngs: whitelist,
   nonExplicitSupportedLngs: true,
   debug: !!debug.translation,
