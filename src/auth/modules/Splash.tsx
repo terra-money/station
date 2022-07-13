118401db60568e274ab5b271d2bf8bfd60a5e809
Samuel Kim <samuel@kernellabs.co>
2022-07-13 17:03:05 +0900

    Add setting body class with load splash

--- src/auth/modules/Splash.tsx
+++ src/auth/modules/Splash.tsx
@@ -1,19 +1,21 @@
+import { useLayoutEffect } from "react"
 import { useTranslation } from "react-i18next"
 import { useTheme, useThemeAnimation } from "data/settings/Theme"
 import { FlexColumn } from "components/layout"
 import { sandbox } from "../scripts/env"
 import styles from "./Splash.module.scss"
-import classNames from "classnames/bind"
-
-const cx = classNames.bind(styles)
 
 const Splash = () => {
   const { t } = useTranslation()
   const animation = useThemeAnimation()
   const { name } = useTheme()
 
+  useLayoutEffect(() => {
+    if (name) document.body.classList.add(name)
+  }, [])
+
   return !sandbox ? null : (
-    <FlexColumn gap={20} className={cx(styles.screen, { [name]: true })}>
+    <FlexColumn gap={20} className={styles.screen}>
       <img src={animation} alt="" width={80} height={80} />
       <h1>{t("Initializing app...")}</h1>
     </FlexColumn>
