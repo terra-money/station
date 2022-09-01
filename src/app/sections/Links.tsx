bd8b459a13741710420bd2d6fec44c1cd6c0a01f
sim <sim@terra.money>
2022-09-01 15:54:25 +0900

    Link community for classic network

--- src/app/sections/Links.tsx
+++ src/app/sections/Links.tsx
@@ -1,20 +1,28 @@
 import { useTranslation } from "react-i18next"
 import DescriptionIcon from "@mui/icons-material/Description"
 import { TUTORIAL } from "config/constants"
+import { useIsClassic } from "data/query"
 import { ExternalLink } from "components/general"
 import { Contacts } from "components/layout"
 import styles from "./Links.module.scss"
 
-const community = {
-  medium: "https://medium.com/terra-money",
-  discord: "https://terra.sc/discord",
-  telegram: "https://t.me/TerraNetworkLobby",
-  twitter: "https://twitter.com/terra_money",
-  github: "https://github.com/terra-money",
-}
-
 const Links = () => {
   const { t } = useTranslation()
+  const isClassic = useIsClassic()
+
+  const community = isClassic
+    ? {
+        discord: "https://terra.sc/classicdiscord",
+        telegram: "https://t.me/TerraLunaChat",
+        twitter: "https://twitter.com/terrac_money",
+      }
+    : {
+        medium: "https://medium.com/terra-money",
+        discord: "https://terra.sc/discord",
+        telegram: "https://t.me/TerraNetworkLobby",
+        twitter: "https://twitter.com/terra_money",
+        github: "https://github.com/terra-money",
+      }
 
   return (
     <div className={styles.links}>
