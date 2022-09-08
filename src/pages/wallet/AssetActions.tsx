5690dc58ba0af0072cda1d6383d5088ca22adc79
sim <sim@terra.money>
2022-09-08 18:46:27 +0900

    Revert swap button on wallet menu

--- src/pages/wallet/AssetActions.tsx
+++ src/pages/wallet/AssetActions.tsx
@@ -8,8 +8,10 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos"
 import { isDenomTerraNative } from "@terra.kitchen/utils"
 import { has } from "utils/num"
 import { useIsClassic } from "data/query"
+import { useNetworkName } from "data/wallet"
 import { useIsWalletEmpty } from "data/queries/bank"
 import { useCW20Pairs } from "data/Terra/TerraAssets"
+import { useTFMTokens } from "data/external/tfm"
 import { InternalButton, InternalLink } from "components/general"
 import { ExtraActions } from "components/layout"
 import { ModalButton } from "components/feedback"
@@ -23,6 +25,7 @@ const AssetActions = ({ token, symbol, balance }: Props) => {
 
   const isWalletEmpty = useIsWalletEmpty()
   const isClassic = useIsClassic()
+  const networkName = useNetworkName()
   const getIsSwappableToken = useGetIsSwappableToken()
 
   return is.mobile() ? (
@@ -61,7 +64,7 @@ const AssetActions = ({ token, symbol, balance }: Props) => {
         {t("Send")}
       </InternalLink>
 
-      {isClassic && (
+      {networkName !== "testnet" && (
         <InternalLink
           icon={<RestartAltIcon style={{ fontSize: 18 }} />}
           to="/swap"
@@ -81,12 +84,21 @@ export default AssetActions
 
 /* helpers */
 const useGetIsSwappableToken = () => {
+  const networkName = useNetworkName()
+  const isClassic = useIsClassic()
   const { data: pairs } = useCW20Pairs()
+  const { data: TFMTokens } = useTFMTokens()
 
   return (token: TerraAddress) => {
     if (isDenomTerraNative(token)) return true
-    if (!pairs) return false
-    const terraswapAvailableList = uniq(flatten(Object.values(pairs)))
-    return terraswapAvailableList.find(({ assets }) => assets.includes(token))
+    if (networkName === "testnet") return false
+    if (isClassic) {
+      if (!pairs) return false
+      const terraswapAvailableList = uniq(flatten(Object.values(pairs)))
+      return terraswapAvailableList.find(({ assets }) => assets.includes(token))
+    } else {
+      if (!TFMTokens) return false
+      return TFMTokens.find(({ contract_addr }) => token === contract_addr)
+    }
   }
 }
