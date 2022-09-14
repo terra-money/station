73145043c4dedccaaaaadcf4cc0c95461e046c2c
sim <sim@terra.money>
2022-09-14 15:10:13 +0900

    Print price impact on swap

--- src/txs/swap/TFMExpectedPrice.tsx
+++ src/txs/swap/TFMExpectedPrice.tsx
@@ -1,14 +1,16 @@
 import { useTranslation } from "react-i18next"
-import { Read } from "components/token"
+import { Read, ReadPercent } from "components/token"
 import { SwapAssets } from "./useSwapUtils"
 import { SwapSpread } from "./SingleSwapContext"
 import { useTFMSwap } from "./TFMSwapContext"
 import Price from "./components/Price"
 
-interface Props extends SwapAssets, Partial<SwapSpread> {}
+interface Props extends SwapAssets, Partial<SwapSpread> {
+  price_impact: number
+}
 
 const TFMExpectedPrice = (props: Props) => {
-  const { askAsset, price, minimum_receive } = props
+  const { askAsset, price, minimum_receive, price_impact } = props
   const { t } = useTranslation()
   const { findDecimals } = useTFMSwap()
 
@@ -27,6 +29,10 @@ const TFMExpectedPrice = (props: Props) => {
           token={askAsset}
           decimals={findDecimals(askAsset)}
         />
+      </dd>
+      <dt>{t("Price impact")}</dt>
+      <dd>
+        <ReadPercent>{price_impact}</ReadPercent>
       </dd>
     </dl>
   )
