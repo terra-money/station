0bd20285a86dbb80ef3f69542299fec8a39f2824
plubber <51789398+ericHgorski@users.noreply.github.com>
2022-12-15 02:38:54 -0600

    Fix: use Station icon as val profile icon (#151)
    * fix: use station icon as val icon
    
    * Use useThemeFavicon
    
    Use `useThemeFavicon()` to change the color of the station logo based on the selected theme.
    
    * Delete Station.svg
    
    Co-authored-by: Alessandro Candeago <54709706+alecande11@users.noreply.github.com>

--- src/pages/stake/components/ProfileIcon.tsx
+++ src/pages/stake/components/ProfileIcon.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
-import { ReactComponent as Terra } from "styles/images/Terra.svg"
 import styles from "./ProfileIcon.module.scss"
+import { useThemeFavicon } from "data/settings/Theme"
 
 interface Props {
   src?: string
@@ -9,8 +9,10 @@ interface Props {
 
 const ProfileIcon = ({ src, size }: Props) => {
   const [error, setError] = useState(false)
+  const stationIcon = useThemeFavicon()
+  
   const attrs = { className: styles.icon, width: size, height: size }
-  if (error || !src) return <Terra {...attrs} />
+  if (error || !src) return <img {...attrs} src={stationIcon} alt="" />
   return <img {...attrs} src={src} onError={() => setError(true)} alt="" />
 }
 
