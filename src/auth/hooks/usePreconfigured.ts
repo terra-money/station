ad897e6f4879fd726214ad3b39bf72832f5dfc33
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-11 11:40:55 +0100

    Add preconfigured wallets

--- src/auth/hooks/usePreconfigured.ts
+++ src/auth/hooks/usePreconfigured.ts
@@ -1,5 +1,12 @@
+import { wordsFromAddress } from "utils/bech32"
+import preconfigured from "../../config/preconfigured.json"
+
 const usePreconfigured = () => {
-  return [] as PreconfiguredWallet[]
+  return preconfigured.map(({ mnemonic, address, name }) => ({
+    mnemonic,
+    name,
+    words: { 330: wordsFromAddress(address) },
+  })) as PreconfiguredWallet[]
 }
 
 export default usePreconfigured
