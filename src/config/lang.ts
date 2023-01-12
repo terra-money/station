5944aa67c368b6e2091798ead5a5ffae415d29ca
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-12 09:39:54 +0100

    disable untranslated languages

--- src/config/lang.ts
+++ src/config/lang.ts
@@ -4,15 +4,15 @@ import LanguageDetector from "i18next-browser-languagedetector"
 import { Dictionary } from "ramda"
 import { debug } from "utils/env"
 
-import cs from "locales/cs.json"
+// import cs from "locales/cs.json"
 import de from "locales/de.json"
 import es from "locales/es.json"
 import fr from "locales/fr.json"
 import it from "locales/it.json"
 // import ko from "locales/ko.json"
 import pl from "locales/pl.json"
-import pt from "locales/pt.json"
-import ru from "locales/ru.json"
+// import pt from "locales/pt.json"
+// import ru from "locales/ru.json"
 import zh from "locales/zh.json"
 
 const flatten = (obj: object, initial = {}): Dictionary<string> => {
@@ -27,7 +27,7 @@ const flatten = (obj: object, initial = {}): Dictionary<string> => {
 console.log(pl)
 
 export const Languages = {
-  cs: { value: "cs", label: "Čeština", translation: flatten(cs) },
+  //cs: { value: "cs", label: "Čeština", translation: flatten(cs) },
   de: { value: "de", label: "Deutsch", translation: flatten(de) },
   en: { value: "en", label: "English", translation: {} },
   es: { value: "es", label: "Español", translation: flatten(es) },
@@ -35,8 +35,8 @@ export const Languages = {
   it: { value: "it", label: "Italiano", translation: flatten(it) },
   // ko: { value: "ko", label: "한국어", translation: flatten(ko) },
   pl: { value: "pl", label: "Polski", translation: flatten(pl) },
-  pt: { value: "pt", label: "Português", translation: flatten(pt) },
-  ru: { value: "ru", label: "Русский", translation: flatten(ru) },
+  //pt: { value: "pt", label: "Português", translation: flatten(pt) },
+  //ru: { value: "ru", label: "Русский", translation: flatten(ru) },
   zh: { value: "zh", label: "中文", translation: flatten(zh) },
 }
 
