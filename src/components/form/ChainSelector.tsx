ae9959ca0a8cc36a190936c39af9cd503b430559
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-14 19:44:45 +0100

    Fix chainselector button

--- src/components/form/ChainSelector.tsx
+++ src/components/form/ChainSelector.tsx
@@ -1,7 +1,7 @@
-import { useNetwork } from "data/wallet"
-import { useEffect, useMemo, useState } from "react"
-import styles from "./ChainSelector.module.scss"
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
+import { useNetwork } from 'data/wallet'
+import { useEffect, useMemo, useState } from 'react'
+import styles from './ChainSelector.module.scss'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 
 interface Props {
   chainsList: string[]
@@ -15,11 +15,11 @@ const ChainSelector = ({ chainsList, onChange }: Props) => {
       Object.values(networks)
         .filter((c) => chainsList.includes(c.chainID))
         .sort((a, b) => {
-          if (a.name === "Terra") return -1
-          if (b.name === "Terra") return 1
+          if (a.name === 'Terra') return -1
+          if (b.name === 'Terra') return 1
           return 0
         }),
-    [networks, chainsList]
+    [networks, chainsList],
   )
   const [index, setIndex] = useState(0)
   const [open, setOpen] = useState(false)
@@ -29,26 +29,34 @@ const ChainSelector = ({ chainsList, onChange }: Props) => {
   }, [list, index])
 
   useEffect(() => {
-    onChange(list[index]?.chainID ?? "")
+    onChange(list[index]?.chainID ?? '')
   }, [index]) // eslint-disable-line
 
   return (
     <div className={styles.container}>
-      <button className={styles.selector} onClick={() => setOpen((o) => !o)}>
+      <button
+        className={styles.selector}
+        onClick={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          setOpen((o) => !o)
+        }}
+      >
         <span>
-          <img src={list[index]?.icon} alt={list[index]?.name} />{" "}
+          <img src={list[index]?.icon} alt={list[index]?.name} />{' '}
           {list[index]?.name}
-        </span>{" "}
+        </span>{' '}
         <ArrowDropDownIcon style={{ fontSize: 20 }} className={styles.caret} />
       </button>
       {open && (
         <div className={styles.options}>
           {list.map(({ chainID, name, icon }, i) => (
             <button
-              className={chainID === list[index]?.chainID ? styles.active : ""}
+              className={chainID === list[index]?.chainID ? styles.active : ''}
               key={chainID}
               onClick={(e) => {
                 e.preventDefault()
+                e.stopPropagation()
                 setIndex(i)
                 setOpen(false)
               }}
