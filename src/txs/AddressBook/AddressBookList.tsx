a79c96f96e5ea0e55568390b8215e711f9d4717b
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-20 17:07:49 +0100

    Add your addresses to the address book

--- src/txs/AddressBook/AddressBookList.tsx
+++ src/txs/AddressBook/AddressBookList.tsx
@@ -5,9 +5,14 @@ import { useAddressBook } from "data/settings/AddressBook"
 import { Button } from "components/general"
 import { Grid } from "components/layout"
 import AddAddressBookItem from "./AddAddressBookItem"
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet"
+import ContactsIcon from "@mui/icons-material/Contacts"
 import CustomItem from "./CustomItem"
 import { useState } from "react"
 import { useModal } from "components/feedback"
+import { useInterchainAddresses } from "auth/hooks/useAddress"
+import styles from "./AddressBookList.module.scss"
+import { useNetwork } from "data/wallet"
 
 interface Props {
   onClick: (item: AddressBook) => void
@@ -17,7 +22,46 @@ const AddressBookList = ({ onClick }: Props) => {
   const { t } = useTranslation()
   const { list, remove } = useAddressBook()
   const [open, setOpen] = useState(false)
+  const [openMy, setOpenMy] = useState(false)
+  const addresses = useInterchainAddresses()
+  const networks = useNetwork()
   const close = useModal()
+
+  if (openMy) {
+    return (
+      <Grid gap={12}>
+        <section className={styles.address__table}>
+          <Grid gap={12}>
+            {Object.entries(addresses ?? {}).map(([chain, address]) => {
+              return (
+                <button
+                  className={styles.address__button}
+                  onClick={() => {
+                    onClick({ name: chain, recipient: address })
+                    close()
+                  }}
+                  key={chain}
+                >
+                  <span>
+                    <img
+                      src={networks[chain].icon}
+                      alt={networks[chain].name}
+                    />
+                    {networks[chain].name}
+                  </span>
+                  {truncate(address)}
+                </button>
+              )
+            })}
+          </Grid>
+        </section>
+        <Button onClick={() => setOpenMy(false)}>
+          <ContactsIcon fontSize="inherit" />
+          {t("Use your address book")}
+        </Button>
+      </Grid>
+    )
+  }
 
   return open ? (
     <AddAddressBookItem close={() => setOpen(false)} />
@@ -45,6 +89,10 @@ const AddressBookList = ({ onClick }: Props) => {
         <Button onClick={() => setOpen(true)}>
           <PersonAddOutlinedIcon fontSize="inherit" />
           {t("Add an address")}
+        </Button>
+        <Button onClick={() => setOpenMy(true)}>
+          <AccountBalanceWalletIcon fontSize="inherit" />
+          {t("Use one of your addresses")}
         </Button>
       </Grid>
     </section>
