a5e3774462863fdb35c0a91e2f71f62db4978281
Joshua <joshua.brigati@gmail.com>
2023-01-30 11:54:24 -0600

    removed 'prices by TFM' on Aside

--- src/app/sections/Aside.tsx
+++ src/app/sections/Aside.tsx
@@ -1,13 +1,11 @@
 import { Grid } from "components/layout"
 import Links from "./Links"
 import styles from "./Aside.module.scss"
-import TFMLink from "components/tfm/TFMLink"
 
 const Aside = () => {
   return (
     <Grid gap={20} className={styles.aside}>
       <Links />
-      <TFMLink />
     </Grid>
   )
 }
