370c78bb6c999aff803044aeebede9b6641dbc5e
Joshua <joshua.brigati@gmail.com>
2023-02-06 11:45:27 -0600

    added a dropdown for each network type that will open to display all of the chains on that specific network type

--- src/components/layout/SettingsSelector.tsx
+++ src/components/layout/SettingsSelector.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
+import { useTranslation } from "react-i18next"
+import { useNetworks } from "app/InitNetworks"
+import classNames from "classnames/bind"
+import { Tooltip } from "components/display"
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"
 import Flex from "./Flex"
 import styles from "./SettingsSelector.module.scss"
 
+const cx = classNames.bind(styles)
 interface Props {
   value: string
   options: { value: string; label: string }[]
@@ -9,29 +16,81 @@ interface Props {
 }
 
 const SettingsSelector = ({ value, options, onChange, withSearch }: Props) => {
+  const { t } = useTranslation()
+
   const selected = value
+  const [openAcc, setOpenAcc] = useState(0)
+
+  const handleClick = (index: any, e: any) => {
+    e.stopPropagation()
+    setOpenAcc(index === openAcc ? 0 : index)
+  }
+
+  const { networks }: { networks: any } = useNetworks()
 
   return (
     <div className={styles.wrapper}>
       <section className={styles.selector}>
-        {options.map(({ value, label }) => (
-          <button
-            key={value}
-            className={styles.item}
-            onClick={() => onChange(value)}
-          >
-            {label}
-            <Flex className={styles.track}>
-              <span
-                className={
-                  selected === value
-                    ? styles.indicator__checked
-                    : styles.indicator
-                }
-              />
-            </Flex>
-          </button>
-        ))}
+        {options.map(({ value, label }, index) => {
+          const networksOnSelection = networks[value] || {}
+          return (
+            <div
+              className={cx(
+                styles.accordion,
+                openAcc === index + 1 ? "opened" : ""
+              )}
+            >
+              <button
+                key={value}
+                className={styles.item}
+                onClick={() => onChange(value)}
+              >
+                <div className={styles.icons_container}>
+                  {label}
+                  {Object.keys(networksOnSelection).length > 1 && (
+                    <Tooltip content={t("View active chains")}>
+                      <KeyboardArrowDownIcon
+                        className={styles.icon}
+                        onClick={(e) => handleClick(index + 1, e)}
+                      />
+                    </Tooltip>
+                  )}
+                </div>
+                <Flex className={styles.track}>
+                  <span
+                    className={
+                      selected === value
+                        ? styles.indicator__checked
+                        : styles.indicator
+                    }
+                  />
+                </Flex>
+              </button>
+              <div
+                className={cx(
+                  styles.content,
+                  openAcc === index + 1 ? "opened" : ""
+                )}
+              >
+                {Object.keys(networksOnSelection).length > 1 &&
+                  Object.keys(networksOnSelection).map((network: any) => {
+                    return (
+                      <div
+                        className={styles.network}
+                        key={networksOnSelection[network].chainID}
+                      >
+                        <img
+                          src={networksOnSelection[network].icon}
+                          alt={networksOnSelection[network].name}
+                        />
+                        {networksOnSelection[network].name}
+                      </div>
+                    )
+                  })}
+              </div>
+            </div>
+          )
+        })}
       </section>
     </div>
   )
