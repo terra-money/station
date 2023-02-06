b76e4c3a3c8f5acd178f37336e4619d40f99587a
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-02-06 21:39:12 +0100

    Hide external links

--- src/pages/gov/ProposalDescription.tsx
+++ src/pages/gov/ProposalDescription.tsx
@@ -1,81 +1,117 @@
+import { TooltipIcon } from "components/display"
+import { Toggle } from "components/form"
+import { ExternalLink } from "components/general"
 import { ProposalResult } from "data/queries/gov"
-import React from "react"
+import React, { useState } from "react"
+import { useTranslation } from "react-i18next"
 
 const ProposalDescription = ({ proposal }: { proposal: ProposalResult }) => {
+  const [showLinks, setShowLinks] = useState(false)
+  const { t } = useTranslation()
   if (!proposal.content) return null
   const { description } = proposal.content
 
-  return <article>{parseText(description)}</article>
+  const parseText = (text: string) => {
+    const result: (React.ReactNode | string)[] = []
+    const lines = text.split(/\\n|\n/)
+
+    lines.forEach((line, i) => {
+      // empty lines
+      //if (!line) return
+
+      if (line.startsWith("# ")) {
+        result.push(<h1 key={i}>{line.replace("# ", "")}</h1>)
+      } else if (line.startsWith("## ")) {
+        result.push(<h2 key={i}>{line.replace("## ", "")}</h2>)
+      } else if (line.startsWith("### ")) {
+        result.push(<h3 key={i}>{line.replace("### ", "")}</h3>)
+      } else if (line.startsWith("- ")) {
+        result.push(<li key={i}>{line.replace("- ", "")}</li>)
+      } else {
+        const lineResult: (React.ReactNode | string)[] = []
+        const pattern =
+          /\*\*(.*?)\*\*|`(.*?)`|(https?:\/\/[^\s]+)|\[(.*?)\]\((https?:\/\/[^\s]+)\)/g
+        let match = pattern.exec(line)
+        let lastIndex = 0
+
+        while (match) {
+          lineResult.push(line.slice(lastIndex, match.index))
+          if (match[1]) {
+            // bold text
+            lineResult.push(<b key={`${i}-${lineResult.length}`}>{match[1]}</b>)
+          } else if (match[2]) {
+            // code
+            lineResult.push(
+              <code key={`${i}-${lineResult.length}`}>{match[2]}</code>
+            )
+          } else if (match[3]) {
+            // plain URL
+            lineResult.push(
+              showLinks || isLinkSafe(match[3]) ? (
+                <ExternalLink
+                  href={match[3]}
+                  icon
+                  key={`${i}-${lineResult.length}`}
+                  style={{
+                    color: isLinkSafe(match[3]) ? undefined : "var(--danger)",
+                  }}
+                >
+                  {match[3]}
+                </ExternalLink>
+              ) : (
+                <TooltipIcon
+                  content={t(`This link has been hidden for security reasons.`)}
+                >
+                  <i>{t("hidden link")}</i>
+                </TooltipIcon>
+              )
+            )
+          } else if (match[4] && match[5]) {
+            result.push(
+              <ExternalLink
+                href={match[5]}
+                icon
+                key={`${i}-${lineResult.length}`}
+              >
+                {match[4]}
+              </ExternalLink>
+            )
+          }
+          lastIndex = pattern.lastIndex
+          match = pattern.exec(line)
+        }
+        lineResult.push(line.slice(lastIndex))
+
+        result.push(<p key={i}>{lineResult}</p>)
+        result.push(<br key={`${i}-br`} />)
+      }
+    })
+
+    return result
+  }
+
+  return (
+    <>
+      <section>
+        <Toggle checked={showLinks} onChange={() => setShowLinks((l) => !l)}>
+          {t("Show hidden links")}
+        </Toggle>
+      </section>
+      <article>{parseText(description)}</article>
+    </>
+  )
 }
 
 export default ProposalDescription
 
-const parseText = (text: string) => {
-  const result: (React.ReactNode | string)[] = []
-  const lines = text.split(/\\n|\n/)
-
-  lines.forEach((line, i) => {
-    // empty lines
-    //if (!line) return
-
-    if (line.startsWith("# ")) {
-      result.push(<h1 key={i}>{line.replace("# ", "")}</h1>)
-    } else if (line.startsWith("## ")) {
-      result.push(<h2 key={i}>{line.replace("## ", "")}</h2>)
-    } else if (line.startsWith("### ")) {
-      result.push(<h3 key={i}>{line.replace("### ", "")}</h3>)
-    } else if (line.startsWith("- ")) {
-      result.push(<li key={i}>{line.replace("- ", "")}</li>)
-    } else {
-      const lineResult: (React.ReactNode | string)[] = []
-      const pattern =
-        /\*\*(.*?)\*\*|`(.*?)`|(https?:\/\/[^\s]+)|\[(.*?)\]\((https?:\/\/[^\s]+)\)/g
-      let match = pattern.exec(line)
-      let lastIndex = 0
-
-      while (match) {
-        lineResult.push(line.slice(lastIndex, match.index))
-        if (match[1]) {
-          // bold text
-          lineResult.push(<b key={`${i}-${lineResult.length}`}>{match[1]}</b>)
-        } else if (match[2]) {
-          // code
-          lineResult.push(
-            <code key={`${i}-${lineResult.length}`}>{match[2]}</code>
-          )
-        } else if (match[3]) {
-          // plain URL
-          lineResult.push(
-            <a
-              key={`${i}-${lineResult.length}`}
-              href={match[3]}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {match[3]}
-            </a>
-          )
-        } else if (match[4] && match[5]) {
-          result.push(
-            <a
-              key={`${i}-${lineResult.length}`}
-              href={match[5]}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {match[4]}
-            </a>
-          )
-        }
-        lastIndex = pattern.lastIndex
-        match = pattern.exec(line)
-      }
-      lineResult.push(line.slice(lastIndex))
-
-      result.push(<p key={i}>{lineResult}</p>)
-      result.push(<br key={`${i}-br`} />)
-    }
-  })
-
-  return result
+function isLinkSafe(url: string) {
+  try {
+    const { protocol, hostname } = new URL(url)
+    return (
+      protocol === "https:" &&
+      (hostname === "terra.money" || hostname.endsWith(".terra.money"))
+    )
+  } catch (e) {
+    return false
+  }
 }
