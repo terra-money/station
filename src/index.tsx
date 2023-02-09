0ff087273b0b9f7eac7d6672ad053c468aad4dfa
liskk <1056509662@qq.com>
2023-02-09 18:36:05 +0800

    feat: add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -28,6 +28,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles"
 
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
+import { getAnalytics, logEvent } from "firebase/analytics"
+import { initializeApp } from "firebase/app"
 
 const connectorOpts = { bridge: BRIDGE }
 
@@ -53,17 +55,43 @@ const theme = createTheme({
     borderRadius: 50,
   },
 })
-if(process.env.REACT_APP_NODE_ENV==='production'){
-  Sentry.init({
-    dsn: "https://904bbe6a902b44279a758d334465d116@o1162849.ingest.sentry.io/4504648930295808",
-    integrations: [new BrowserTracing()],
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-    ignoreErrors:['UnhandledRejection']
-  });
-}
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA15UjL8TFIHLWUk-S83KeuLRC_D7hvwUU",
+  authDomain: "mises-official-site.firebaseapp.com",
+  projectId: "mises-official-site",
+  storageBucket: "mises-official-site.appspot.com",
+  messagingSenderId: "235777024442",
+  appId: "1:235777024442:web:da94196c84a941fab07d83",
+  measurementId: "G-Y5Y02HDCC8",
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+Sentry.init({
+  enabled: process.env.REACT_APP_NODE_ENV==='production',
+  dsn:
+    "https://904bbe6a902b44279a758d334465d116@o1162849.ingest.sentry.io/4504648930295808",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  ignoreErrors:['UnhandledRejection'],
+  beforeSend: (event, hint) => {
+    if (hint.originalException?.toString() === "Error: Failed to fetch") {
+      logEvent(analytics, "portal_error", {
+        error_message: hint.originalException?.toString(),
+      });
+
+      return null;
+    }
+    console.log(event);
+    return event;
+  },
+});
+
 root.render(
   <StrictMode>
     <RecoilRoot>
