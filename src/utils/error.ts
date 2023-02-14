ce800819a67ce8fe113bb030f588b87c539938b9
plubber <er.h.gorski@gmail.com>
2023-02-14 12:53:38 -0500

    feat: generalize error msg on prod

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -8,5 +8,8 @@ export const getErrorMessage = (
   if (axios.isAxiosError(error))
     return (error as AxiosError<any>).response?.data?.message ?? error.message
 
-  if (error instanceof Error) return error.message
+  if (error instanceof Error && process.env.NODE_ENV === "development")
+    return error.message
+
+  return "Something went wrong, reload the page and try again"
 }
