98a8bc17ba33d86a6dce9bfb77d8e1e676b25b87
Rodion Chachura <geekrodion@gmail.com>
2023-02-28 19:57:35 +0400

    update: only Terra available on classic

--- src/txs/swap/SwapProvidersContext.tsx
+++ src/txs/swap/SwapProvidersContext.tsx
@@ -1,6 +1,6 @@
 import { useTFMChains } from "data/external/multichainTfm"
 import { getChainTokens, useRangoMeta } from "data/external/rango"
-import { useNetwork } from "data/wallet"
+import { useNetwork, useNetworkName } from "data/wallet"
 import { PropsWithChildren, useMemo } from "react"
 import createContext from "utils/createContext"
 
@@ -13,12 +13,23 @@ interface SwapProviders {
 export const [useSwapProviders, SwapProvidersProvider] =
   createContext<SwapProviders>("useSwapProviders")
 
-const unsupportedChains = ["osmosis-1"]
+const supportedChainsByNetwork: Record<string, string[]> = {
+  mainnet: ["juno-1", "phoenix-1", "kaiyo-1"],
+  classic: ["columbus-5"],
+}
 
 export const SwapProvidersContext = ({ children }: PropsWithChildren<{}>) => {
   const { data: rangoMeta } = useRangoMeta()
   const { data: tfmChains } = useTFMChains()
   const networks = useNetwork()
+  const networkName = useNetworkName()
+
+  const supportedChains = useMemo(() => {
+    const supportedChainsOnNetwork = supportedChainsByNetwork[networkName]
+    if (!supportedChainsOnNetwork) return []
+
+    return supportedChainsOnNetwork.filter((chainId) => networks[chainId])
+  }, [networkName, networks])
 
   const providers = useMemo(() => {
     const result: Record<string, SwapProvider[]> = {}
@@ -30,7 +41,7 @@ export const SwapProvidersContext = ({ children }: PropsWithChildren<{}>) => {
       result[chain].push(provider)
     }
 
-    const stationChains = new Set(Object.keys(networks))
+    const stationChains = new Set(supportedChains)
 
     if (rangoMeta) {
       rangoMeta.blockchains.forEach(({ chainId }) => {
@@ -53,12 +64,12 @@ export const SwapProvidersContext = ({ children }: PropsWithChildren<{}>) => {
       })
     }
 
-    unsupportedChains.forEach((chainId) => {
-      delete result[chainId]
-    })
-
     return result
-  }, [networks, rangoMeta, tfmChains])
+  }, [rangoMeta, supportedChains, tfmChains])
+
+  if (Object.keys(providers).length === 0) {
+    return <p>Swap is not available for this network</p>
+  }
 
   return (
     <SwapProvidersProvider value={{ providers }}>
