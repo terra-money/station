1274e12a48a98606a06aa53739d605ea04324d57
Jason Stallings <jason@stallin.gs>
2023-02-28 11:38:07 -0600

    wip: Mock useWallet and react-i18next.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,42 @@
 import '@testing-library/jest-dom';
-import { Crypto } from "@peculiar/webcrypto"
-global.crypto = new Crypto()
+import { Crypto } from '@peculiar/webcrypto';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+global.crypto = new Crypto();
 
 jest.mock('react-modal', () => ({
-    ...jest.requireActual('react-modal'),
-    setAppElement: () => { },
+  ...jest.requireActual('react-modal'),
+  setAppElement: () => {},
 }));
 
 jest.mock('@ledgerhq/hw-transport-web-ble', () => ({}));
+
+jest.mock('react-query', () => ({
+  useQueryClient: () => {
+    return {};
+  },
+}));
+
+jest.mock('@terra-money/use-wallet', () => ({
+  useConnectedWallet: () => {
+    return {};
+  },
+}));
+
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  },
+}));
