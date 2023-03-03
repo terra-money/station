11a5810b247ac0f7283c18ecec50a8c19d3186aa
Mijolae <mijolae@gmail.com>
2023-03-03 11:19:20 -0600

    updating languagesetting test

--- src/app/sections/LanguageSetting.test.tsx
+++ src/app/sections/LanguageSetting.test.tsx
@@ -1,10 +1,24 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import LanguageSetting from "./LanguageSetting"
+import { NetworksProvider } from "app/InitNetworks"
 
+type TokenFilter = <T>(network: Record<string, T>) => Record<string, T>
 describe("LanguageSetting", () => {
-  it("renders Language Selection", () => {
-    render(<LanguageSetting />)
+  it("render((s Language Selection", () => {
+    const networks = {} as jest.Mocked<InterchainNetworks>
+    const mockedTokenFilter = {} as jest.Mocked<TokenFilter>
+    render(
+      <NetworksProvider
+        value={{
+          networks: networks,
+          filterEnabledNetworks: mockedTokenFilter,
+          filterDisabledNetworks: mockedTokenFilter,
+        }}
+      >
+        <LanguageSetting />
+      </NetworksProvider>
+    )
     expect(screen.getByText("Deutsch")).toBeInTheDocument()
     expect(screen.getByText("Français")).toBeInTheDocument()
   })
