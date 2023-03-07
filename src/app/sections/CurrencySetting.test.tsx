5c3071c23e12bfb1bc3dacf0a03866838e0daade
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-03-07 13:49:07 +0700

    feat: user event testing
    Test user event changes after render with snapshot test.

--- src/app/sections/CurrencySetting.test.tsx
+++ src/app/sections/CurrencySetting.test.tsx
@@ -4,6 +4,7 @@ import CurrencySetting from "./CurrencySetting"
 import { RecoilRoot } from "recoil"
 import { NetworksProvider } from "../InitNetworks"
 import renderer from "react-test-renderer"
+import user from "@testing-library/user-event"
 
 type TokenFilter = <T>(network: Record<string, T>) => Record<string, T>
 const networks = {} as jest.Mocked<InterchainNetworks>
@@ -65,15 +66,18 @@ describe("Currency Setting loads", () => {
   it("only currencies in supported fiat list display", () => {
     renderComponent()
 
+    // Ensure all currencies in supported fiat list are displayed.
     for (let { name, symbol } of supportedFiatListMock) {
       let currencyDisplay = screen.getByText(`${symbol} - ${name}`)
       expect(currencyDisplay).toBeInTheDocument()
     }
 
+    // Ensure only supported fiat list currencies are on display.
     const buttons = screen.getAllByRole("button")
     expect(buttons).toHaveLength(3)
   })
 
+  // Ensure snapshot matches original component.
   it("matches snapshot", () => {
     const tree = renderer
       .create(
@@ -91,5 +95,23 @@ describe("Currency Setting loads", () => {
       )
       .toJSON()
     expect(tree).toMatchSnapshot()
+  })
+})
+
+describe("User can change currencies", () => {
+  it("allows user to change currencies", async () => {
+    const { asFragment } = renderComponent()
+
+    // Search for JPY currency in search bar.
+    const searchBar = screen.getByRole("textbox")
+    await user.click(searchBar)
+    await user.keyboard("JPY")
+
+    // Ensure only JPY button shows and click to change currency.
+    const jpyButton = screen.getByRole("button")
+    await user.click(jpyButton)
+
+    // Ensure snapshot matches user augmented component.
+    expect(asFragment()).toMatchSnapshot()
   })
 })
