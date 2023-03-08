1c5eaead2dbf96794b247a9371b66b35875e5819
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-03-08 14:12:34 +0700

    feat: LanguageSetting test updates (#301)

--- src/app/__tests__/LanguageSetting.test.tsx
+++ src/app/__tests__/LanguageSetting.test.tsx
@@ -2,24 +2,42 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import LanguageSetting from "../sections/LanguageSetting"
 import { NetworksProvider } from "app/InitNetworks"
+import user from "@testing-library/user-event"
 
-type TokenFilter = <T>(network: Record<string, T>) => Record<string, T>
-describe("LanguageSetting", () => {
-  it("render((s Language Selection", () => {
-    const networks = {} as jest.Mocked<InterchainNetworks>
-    const mockedTokenFilter = {} as jest.Mocked<TokenFilter>
-    render(
-      <NetworksProvider
-        value={{
-          networks: networks,
-          filterEnabledNetworks: mockedTokenFilter,
-          filterDisabledNetworks: mockedTokenFilter,
-        }}
-      >
-        <LanguageSetting />
-      </NetworksProvider>
-    )
-    expect(screen.getByText("Deutsch")).toBeInTheDocument()
-    expect(screen.getByText("Français")).toBeInTheDocument()
+function renderComponent() {
+  type TokenFilter = <T>(network: Record<string, T>) => Record<string, T>
+  const networks = {} as jest.Mocked<InterchainNetworks>
+  const mockedTokenFilter = {} as jest.Mocked<TokenFilter>
+
+  return render(
+    <NetworksProvider
+      value={{
+        networks: networks,
+        filterEnabledNetworks: mockedTokenFilter,
+        filterDisabledNetworks: mockedTokenFilter,
+      }}
+    >
+      <LanguageSetting />
+    </NetworksProvider>
+  )
+}
+
+describe("LanguageSetting component matches snapshots", () => {
+  it("matches original component", () => {
+    const { asFragment } = renderComponent()
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("matches user augmented component", async () => {
+    const { asFragment } = renderComponent()
+
+    // Change language to Chinese.
+    const chineseLanguage = screen.getByRole("button", {
+      name: "中文",
+    })
+    await user.click(chineseLanguage)
+
+    // Ensure snapshot matches user augmented component.
+    expect(asFragment()).toMatchSnapshot()
   })
 })
