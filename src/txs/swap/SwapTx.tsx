94f8ae691b671e7686128e734987b99f930554fb
Michael Merz <mwmerz@protonmail.com>
2023-03-13 18:08:37 -0400

    wip: todos

--- src/txs/swap/SwapTx.tsx
+++ src/txs/swap/SwapTx.tsx
@@ -4,6 +4,40 @@ import { Wrong } from "components/feedback"
 import { ExternalLink } from "components/general"
 import { useNetworkName } from "data/wallet"
 
+// TODO
+// - [ ]  Create new route for swap
+//     - [ ]  provide all chains in chain selector
+// - [ ]  Create new context based on selected chain
+//     - [ ]  fetch tfm + rango available swaps for selected chain
+//     - [ ]  fetch native denoms
+//     - [ ]  fetch custom cw20s
+//     - [ ]  filter native + custom through whitelists
+//     - [ ]  fetch balances for native + custom
+//     - [ ]  populate map of trades key’d by offer asset, track tfm or rango
+// - [ ]  Create new form to receive context and chain info
+//     - Render
+//         - [ ]  input 1 (offer asset)
+//             - [ ]  show assets with balance or ALL
+//         - [ ]  input 2 (receive asset)
+//             - [ ]  populated with trade lookup
+//         - [ ]  slippage (text)
+//         - [ ]  submit button
+//     - Behavior
+//         - [ ]  on input update, (assets or amount)
+//             - [ ]  simulate trade for each network available
+//                 - [ ]  pick trade with highest receive asset amount
+//                 - [ ]  update “powered by” link based on swap network
+//             - [ ]  if simulate is successful AND user has balance of gas denom
+//                 - [ ]  allow submit
+//         - [ ]  on submit
+//             - [ ]  perform tx, process success or error
+//     - UX
+//         - [ ]  Provide option to get gas denom on error?
+//             - [ ]  link out
+//             - [ ]  fee abstraction
+//                 - [ ]  swap for local(terra) ibc equiv and send?
+//                 - [ ]  ibc-hooks
+
 const SwapTx = () => {
   const { t } = useTranslation()
   const networkName = useNetworkName()
