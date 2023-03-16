c0c67009258742661466e15a5a89cce63a19ee42
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-03-17 00:32:37 +0900

    feat: isError utility function
    Adding `isError` utility function to evaluate if `error` value is true or false.  Currently, values of `error` that are empty objects are returning as true when they should be false.  `isError` includes this empty object check.

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios"
 
-export const getErrorMessage = (
-  error?: Error | AxiosError | unknown
+const getErrorMessage = (
+  error?: Error | AxiosError | object | unknown
 ): string | undefined => {
   if (!error) return
 
@@ -10,3 +10,17 @@ export const getErrorMessage = (
 
   if (error instanceof Error) return error.message
 }
+
+const isError = (error?: Error | AxiosError | object | unknown): boolean => {
+  if (
+    !error ||
+    (Object.getPrototypeOf(error) === Object.prototype &&
+      Object.keys(error).length === 0)
+  ) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export { getErrorMessage, isError }
