3358332e8431445e5f6be3d366f8a813ea300e2e
plubber <er.h.gorski@gmail.com>
2023-03-22 16:17:58 -0400

    fix: style updates

--- src/pages/wallet/FiatRampModal.tsx
+++ src/pages/wallet/FiatRampModal.tsx
@@ -2,6 +2,7 @@ import { FIAT_RAMP, KADO_API_KEY } from "config/constants"
 import { useAddress } from "data/wallet"
 import { useState } from "react"
 import { LoadingCircular } from "components/feedback"
+import styles from "./FiatRampModal.module.scss"
 
 const FiatRampModal = () => {
   const address = useAddress()
@@ -11,23 +12,26 @@ const FiatRampModal = () => {
     network: "Terra",
     onToAddress: address || "",
     apiKey: KADO_API_KEY,
+    product: "BUY",
+    onRevCurrency: "USDC",
   }
 
   const kadoUrlParams = new URLSearchParams(rampParams).toString()
 
   return (
-    <>
-      {isLoading && <LoadingCircular size={36} thickness={2} />}
-      {
-        <iframe
-          src={`${FIAT_RAMP}?${kadoUrlParams}`}
-          width="500"
-          height="686"
-          title="Kado Ramp"
-          onLoad={() => setIsLoading(false)}
-        />
-      }
-    </>
+    <div className={styles.container}>
+      {isLoading && (
+        <div className={styles.loading}>
+          <LoadingCircular size={36} thickness={2} />
+        </div>
+      )}
+      <iframe
+        className={styles.iframe}
+        src={`${FIAT_RAMP}?${kadoUrlParams}`}
+        title="Kado Ramp"
+        onLoad={() => setIsLoading(false)}
+      />
+    </div>
   )
 }
 
