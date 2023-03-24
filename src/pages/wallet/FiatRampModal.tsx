9a435dac3243bb10992f2723daf685a3abc01f05
plubber <er.h.gorski@gmail.com>
2023-03-24 10:48:31 -0400

    cleanup

--- src/pages/wallet/FiatRampModal.tsx
+++ src/pages/wallet/FiatRampModal.tsx
@@ -3,20 +3,29 @@ import { useAddress } from "data/wallet"
 import { useState } from "react"
 import { LoadingCircular } from "components/feedback"
 import styles from "./FiatRampModal.module.scss"
+import { useTheme } from "data/settings/Theme"
+import qs from "qs"
 
 const FiatRampModal = () => {
   const address = useAddress()
   const [isLoading, setIsLoading] = useState(true)
+  const { name: theme } = useTheme()
+
+  if (!address) return null
 
   const rampParams = {
     network: "Terra",
-    onToAddress: address || "",
+    onToAddress: address,
     apiKey: KADO_API_KEY,
     product: "BUY",
     onRevCurrency: "USDC",
+    networkList: ["TERRA", "OSMOSIS", "KUJIRA", "JUNO"].join(","),
+    productList: ["BUY", "SELL"].join(","),
+    theme,
+    cryptoList: ["USDC"].join(","),
   }
 
-  const kadoUrlParams = new URLSearchParams(rampParams).toString()
+  const kadoUrlParams = qs.stringify(rampParams)
 
   return (
     <div className={styles.container}>
