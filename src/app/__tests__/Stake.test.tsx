03a090d1653dbc3c1b015d20acd8f24dec4cbd16
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-04-05 17:17:09 +0200

    feat: staking page test (#330)

---
+++ src/app/__tests__/Stake.test.tsx
@@ -0,0 +1,156 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import Stake from "pages/stake/Stake"
+import { NetworksProvider } from "app/InitNetworks"
+import { RecoilRoot } from "recoil"
+import { MemoryRouter } from "react-router-dom"
+import { mockNetworks } from "./__mocks__/Networks.mock"
+import { mockValidators } from "./__mocks__/Validators.mock"
+import { mockDelegations } from "./__mocks__/Delegations.mock"
+import { mockCalcDelegations } from "./__mocks__/CalcDelegations.mock"
+import { mockUnbondings } from "./__mocks__/Unbondings.mock"
+import { mockRewards } from "./__mocks__/Rewards.mock"
+import { mockBalances } from "./__mocks__/Balances.mock"
+import user from "@testing-library/user-event"
+
+function renderComponent() {
+  type TokenFilter = <T>(network: Record<string, T>) => Record<string, T>
+  const networks = {} as jest.Mocked<InterchainNetworks>
+  const mockedTokenFilter = () => true as jest.Mocked<TokenFilter>
+
+  return render(
+    <NetworksProvider
+      value={{
+        networks: networks,
+        filterEnabledNetworks: mockedTokenFilter,
+        filterDisabledNetworks: mockedTokenFilter,
+      }}
+    >
+      <MemoryRouter>
+        <RecoilRoot>
+          <Stake />
+        </RecoilRoot>
+      </MemoryRouter>
+    </NetworksProvider>
+  )
+}
+
+jest.mock("../../data/wallet", () => {
+  const mockUseNetwork = () => {
+    return mockNetworks
+  }
+
+  const mockUseChainID = () => {
+    return "pisco-1"
+  }
+
+  return {
+    useNetwork: mockUseNetwork,
+    useChainID: mockUseChainID,
+  }
+})
+
+jest.mock("@terra-money/wallet-provider", () => {
+  const actual = jest.requireActual("@terra-money/wallet-provider")
+
+  const useWallet = () => {
+    return {
+      status: "WALLET_CONNECTED",
+      network: ["pisco-1"],
+    }
+  }
+
+  return {
+    ...actual,
+    useWallet: useWallet,
+  }
+})
+
+jest.mock("../../data/queries/staking", () => {
+  const mockUseInterchainDelegations = () => mockDelegations
+  const mockUseInterchainValidators = () => mockValidators
+  const mockUseCalcDelegationsByValidator = () => mockCalcDelegations
+  const mockUseDelegations = () => mockDelegations
+  const mockUseUnbondings = () => mockUnbondings
+  const mockUseValidators = () => mockValidators
+
+  return {
+    useInterchainDelegations: mockUseInterchainDelegations,
+    useInterchainValidators: mockUseInterchainValidators,
+    useCalcDelegationsByValidator: mockUseCalcDelegationsByValidator,
+    useDelegations: mockUseDelegations,
+    useUnbondings: mockUseUnbondings,
+    useValidators: mockUseValidators,
+  }
+})
+
+jest.mock("../../data/queries/distribution", () => {
+  const mockUseRewards = () => {
+    return mockRewards
+  }
+
+  return {
+    useRewards: mockUseRewards,
+  }
+})
+
+jest.mock("../../data/queries/bank", () => {
+  const mockUseBalances = () => {
+    return mockBalances
+  }
+
+  return {
+    useBalances: mockUseBalances,
+  }
+})
+
+describe("Contract component matches snapshots", async () => {
+  it("matches original component", () => {
+    const { asFragment } = renderComponent()
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("matches undelegate page", async () => {
+    const { asFragment } = renderComponent()
+
+    // Search for JPY currency in search bar.
+    const undelegateButton = screen.getByRole("button", {
+      name: /Undelegate/i,
+    })
+    await user.click(undelegateButton)
+
+    // Ensure snapshot matches user augmented component.
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("matches manual staking page", async () => {
+    const { asFragment } = renderComponent()
+
+    // Search for JPY currency in search bar.
+    const manualStakingButton = screen.getByRole("button", {
+      name: /Manual Stake/i,
+    })
+    await user.click(manualStakingButton)
+
+    // Ensure snapshot matches user augmented component.
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it("matches manual staking page with search", async () => {
+    const { asFragment } = renderComponent()
+
+    // Search for JPY currency in search bar.
+    const manualStakingButton = screen.getByRole("button", {
+      name: /Manual Stake/i,
+    })
+    await user.click(manualStakingButton)
+
+    // Search for validator.
+    const searchBar = screen.getByRole("textbox")
+    await user.click(searchBar)
+    await user.keyboard("Terran")
+
+    // Ensure snapshot matches user augmented component.
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
---
+++ src/app/__tests__/__mocks__/Unbondings.mock.tsx
@@ -0,0 +1,3 @@
+export const mockUnbondings = [
+  '{"delegator_address":"terra18j6kg55a960v0l845mxeg6k0r8vfux5tsexdkp","entries":[{"balance":"89999999","completion_time":"2023-03-29T06:28:35.978Z","creation_height":"4755639","initial_balance":"89999999"}],"validator_address":"terravaloper1gtw2uxdkdt3tvq790ckjz8jm8qgwkdw3uptstn"}',
+]
