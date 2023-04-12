4769dcdf54a45717c1fce5e6e1cb5660c3f58029
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-04-12 13:04:57 +0200

    feat: Wallet component test
    Adding test for wallet component.
    
    Adding mock of `ReactModal` for testing components which utilize it.
    
    Adding `useWallet` mock to tests setup.

---
+++ src/app/__tests__/__mocks__/ReactModal.mock.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+interface IProps {
+  id: string
+  isOpen: boolean
+  contentLabel: string
+  overlayClassName: string
+  className: any
+  shouldCloseOnOverlayClick(): void
+  onRequestClose(): void
+  parentSelector: any
+  onAfterOpen(): void
+}
+
+export default class MockReactModal extends React.Component<IProps, any> {
+  static setAppElement() {
+    return {}
+  }
+
+  render() {
+    return (
+      <div id={this.props.id}>{this.props.isOpen && this.props.children}</div>
+    )
+  }
+}
