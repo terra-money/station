b641af43340f6efb1e10bab85f75a0500c04e630
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-04-19 10:05:45 +0200

    fix: error checking
    Error received may be empty object which is truthy in JavaScript.  Check for falsy values OR empty objects when deciding if an error is truthy.

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,9 +1,14 @@
 import axios, { AxiosError } from "axios"
 
 export const getErrorMessage = (
-  error?: Error | AxiosError | unknown
+  error?: Error | AxiosError | object | unknown
 ): string | undefined => {
-  if (!error) return
+  if (
+    !error ||
+    (Object.getPrototypeOf(error) === Object.prototype &&
+      Object.keys(error as object).length === 0)
+  )
+    return
 
   if (axios.isAxiosError(error))
     return (error as AxiosError<any>).response?.data?.message ?? error.message
