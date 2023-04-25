bb330d2b08c2647021cc83b104a5ab8818f1a75f
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-04-25 14:24:38 +0200

    if "all" is not an option select terra

--- src/components/layout/ChainFilter.tsx
+++ src/components/layout/ChainFilter.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames"
-import { useNetwork } from "data/wallet"
+import { useChainID, useNetwork } from "data/wallet"
 import { useState, memo, useMemo, useRef, useEffect } from "react"
 import { useTranslation } from "react-i18next"
 import styles from "./ChainFilter.module.scss"
@@ -36,6 +36,7 @@ const ChainFilter = ({
   const { displayChains } = useDisplayChains()
   const { selectedDisplayChain, changeSelectedDisplayChain } =
     useSelectedDisplayChain()
+  const terraChainID = useChainID()
   const sortedDisplayChains = useSortedDisplayChains()
 
   useEffect(() => {
@@ -154,7 +155,7 @@ const ChainFilter = ({
 
   useEffect(() => {
     if (selectedChain !== undefined && network[selectedChain] === undefined) {
-      handleSetChain(undefined)
+      handleSetChain(all ? undefined : terraChainID)
     }
   }, [network]) // eslint-disable-line
 
