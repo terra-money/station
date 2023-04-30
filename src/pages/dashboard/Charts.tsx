419aaf94a930b6eef0b79428401e150118d44ab6
Michael Merz <mwmerz@protonmail.com>
2023-04-30 17:11:07 -0400

    chore: linting

--- src/pages/dashboard/Charts.tsx
+++ src/pages/dashboard/Charts.tsx
@@ -1,15 +1,12 @@
 import classNames from "classnames/bind"
-import { useIsClassic } from "data/query"
 import { useIsTerraAPIAvailable } from "data/Terra/TerraAPI"
 import TxVolume from "../charts/TxVolume"
-import StakingReturn from "../charts/StakingReturn"
 import Wallets from "../charts/Wallets"
 import styles from "./Charts.module.scss"
 
 const cx = classNames.bind(styles)
 
 const Charts = () => {
-  const isClassic = useIsClassic()
   const available = useIsTerraAPIAvailable()
   if (!available) return null
 
