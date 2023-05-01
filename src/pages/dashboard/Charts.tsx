16709f51feeade02a3dbf1924d77f44917d697e2
Mike <17463738+mwmerz@users.noreply.github.com>
2023-05-01 06:42:08 -0400

    Feat/remove-staking-return-mobile (#364)
    * feat: remove staking return widget mobile
    
    * chore: linting

--- src/pages/dashboard/Charts.tsx
+++ src/pages/dashboard/Charts.tsx
@@ -1,22 +1,18 @@
 import classNames from "classnames/bind"
-import { useIsClassic } from "data/query"
 import { useIsTerraAPIAvailable } from "data/Terra/TerraAPI"
 import TxVolume from "../charts/TxVolume"
-import StakingReturn from "../charts/StakingReturn"
 import Wallets from "../charts/Wallets"
 import styles from "./Charts.module.scss"
 
 const cx = classNames.bind(styles)
 
 const Charts = () => {
-  const isClassic = useIsClassic()
   const available = useIsTerraAPIAvailable()
   if (!available) return null
 
   return (
     <div className={cx(styles.charts, { trisect: true })}>
       <TxVolume />
-      {!isClassic && <StakingReturn />}
       <Wallets />
     </div>
   )
