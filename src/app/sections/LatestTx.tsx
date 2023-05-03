48f6d793e0ee392bac075c1b2b86fa1cc3e0abc9
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2023-05-03 11:53:42 +0200

    Revert "fix: remove unneeded ts-expect-error"
    This reverts commit 0afc3c2cdfc9b5ae5336fb1d48b38d512bf3929f.

--- src/app/sections/LatestTx.tsx
+++ src/app/sections/LatestTx.tsx
@@ -104,6 +104,7 @@ const TxIndicator = ({ txhash }: { txhash: string }) => {
   const ruleset = createActionRuleSet(networkName)
   const logMatcher = createLogMatcherForActions(ruleset)
   const getCanonicalMsgs = (txInfo: TxInfo) => {
+    // @ts-expect-error
     const matchedMsg = getTxCanonicalMsgs(txInfo, logMatcher)
     return matchedMsg
       ? matchedMsg
