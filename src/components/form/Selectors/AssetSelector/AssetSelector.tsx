119569857b1a69844110c3e00989bd440cf9dc07
Joshua <joshua.brigati@gmail.com>
2023-05-30 14:05:55 -0500

    Created a Asset Selector for Send page with Search

--- src/components/form/Selectors/AssetSelector/AssetSelector.tsx
+++ src/components/form/Selectors/AssetSelector/AssetSelector.tsx
@@ -1,40 +1,34 @@
-import { useMemo, useState, useRef, useEffect } from "react"
-import styles from "./ChainSelector.module.scss"
+import { useState, useRef, useEffect } from "react"
+import styles from "../../ChainSelector.module.scss"
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
-import { useNetworks } from "app/InitNetworks"
-import ChainList from "./ChainList"
+import AssetList from "./AssetList"
+
+interface AssetType {
+  denom: string
+  balance: string
+  icon: string
+  symbol: string
+  price: number
+  chains: string[]
+}
 
 interface Props {
-  chainsList: string[]
+  assetList: AssetType[]
   onChange: (chain: string) => void
   value: string
   small?: boolean
   noSearch?: boolean
 }
 
-const ChainSelector = ({
-  chainsList,
+const AssetSelector = ({
+  assetList,
   onChange,
   value,
   small,
   noSearch,
 }: Props) => {
-  const { networks } = useNetworks()
-  const allNetworks = useMemo(
-    () => ({
-      ...networks.localterra,
-      ...networks.classic,
-      ...networks.testnet,
-      ...networks.mainnet,
-    }),
-    [networks]
-  )
-
-  const list = useMemo(
-    () => chainsList.map((chainID) => allNetworks[chainID]),
-    [allNetworks, chainsList]
-  )
   const [open, setOpen] = useState(false)
+  const [selectedIndex, setSelectedIndex] = useState(0)
   const ref = useRef<HTMLDivElement>(null)
 
   const handleClickOutside = (event: MouseEvent) => {
@@ -49,8 +43,9 @@ const ChainSelector = ({
     }
   }, [])
 
-  const handleSelection = (selectedChain: string) => {
-    onChange(selectedChain)
+  const handleSelection = (denom: string, index: number) => {
+    setSelectedIndex(index)
+    onChange(denom)
     setOpen(false)
   }
 
@@ -65,16 +60,19 @@ const ChainSelector = ({
         }}
       >
         <span>
-          <img src={allNetworks[value]?.icon} alt={allNetworks[value]?.name} />{" "}
-          {allNetworks[value]?.name}
+          <img
+            src={assetList[selectedIndex].icon}
+            alt={assetList[selectedIndex]?.denom}
+          />{" "}
+          {assetList[selectedIndex]?.symbol}
         </span>{" "}
         <ArrowDropDownIcon style={{ fontSize: 20 }} className={styles.caret} />
       </button>
       {open && (
-        <ChainList list={list} onChange={handleSelection} value={value} />
+        <AssetList list={assetList} onChange={handleSelection} value={value} />
       )}
     </div>
   )
 }
 
-export default ChainSelector
+export default AssetSelector
--- src/pages/wallet/SendPage.tsx
+++ src/pages/wallet/SendPage.tsx
@@ -7,8 +7,8 @@ import {
 } from "@terra-money/feather.js"
 import { isDenom, toAmount } from "@terra-money/terra-utils"
 import { useInterchainAddresses } from "auth/hooks/useAddress"
-import { Form, FormItem, FormWarning, Input, Select } from "components/form"
-import ChainSelector from "components/form/ChainSelector"
+import { Form, FormItem, FormWarning, Input } from "components/form"
+import ChainSelector from "components/form/Selectors/ChainSelector/ChainSelector"
 import { Flex, Grid } from "components/layout"
 import { SAMPLE_ADDRESS } from "config/constants"
 import { useBankBalance } from "data/queries/bank"
@@ -31,9 +31,10 @@ import { queryKey } from "data/query"
 import Tx from "txs/Tx"
 import AddressBookList from "txs/AddressBook/AddressBookList"
 import { ModalButton } from "components/feedback"
+import AssetSelector from "components/form/Selectors/AssetSelector/AssetSelector"
 
 interface TxValues {
-  asset: string
+  asset?: string
   chain?: string
   recipient?: string // AccAddress | TNS
   address?: AccAddress // hidden input
@@ -130,6 +131,13 @@ const SendPage = () => {
       chain === watch("chain") &&
       readNativeDenom(denom).token === watch("asset")
   )
+
+  /* resolve asset */
+  useEffect(() => {
+    if (!asset) {
+      setValue("asset", defaultAsset)
+    }
+  }, [form, asset, defaultAsset, setValue])
 
   /* resolve recipient */
   useEffect(() => {
@@ -339,18 +347,11 @@ const SendPage = () => {
                 label={t("Asset")}
                 error={errors.asset?.message ?? errors.address?.message}
               >
-                <Select
-                  {...register("asset", {
-                    value: defaultAsset,
-                  })}
-                  autoFocus
-                >
-                  {availableAssets.map(({ denom, symbol }, i) => (
-                    <option value={denom} key={i}>
-                      {symbol}
-                    </option>
-                  ))}
-                </Select>
+                <AssetSelector
+                  value={asset ?? ""}
+                  onChange={(asset) => setValue("asset", asset)}
+                  assetList={availableAssets}
+                />
               </FormItem>
               {availableChains && (
                 <FormItem label={t("Source chain")}>
