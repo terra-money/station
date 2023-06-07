08c08ec6c270425d707b4a5f68c57eeecf753bec
Michael Merz <mwmerz@protonmail.com>
2023-06-07 17:41:41 -0400

    feat: replace ToNow component with DateTimeRenderer component.

--- src/pages/history/HistoryItem.tsx
+++ src/pages/history/HistoryItem.tsx
@@ -2,7 +2,7 @@ import { Fragment } from "react"
 import { useTranslation } from "react-i18next"
 import { FinderLink } from "components/general"
 import { Card } from "components/layout"
-import { Dl } from "components/display"
+import { Dl, DateTimeRenderer } from "components/display"
 import { ReadMultiple } from "components/token"
 import HistoryMessage from "./HistoryMessage"
 import styles from "./HistoryItem.module.scss"
@@ -73,7 +73,9 @@ const HistoryItem = ({
         </p>
         <p className={styles.time}>
           <DateRangeIcon />
-          <span>{new Date(timestamp).toLocaleString()}</span>
+          <DateTimeRenderer format={"localestring"}>
+            {timestamp}
+          </DateTimeRenderer>
         </p>
       </header>
 
