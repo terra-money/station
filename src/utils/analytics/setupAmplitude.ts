cf26d114763eb11b89f13c67fa18dd292f2d3d21
plubber <er.h.gorski@gmail.com>
2023-07-06 12:34:23 -0400

    chore: fix versioning

--- src/utils/analytics/setupAmplitude.ts
+++ src/utils/analytics/setupAmplitude.ts
@@ -1,6 +1,4 @@
 import * as amplitude from "@amplitude/analytics-browser"
-import packageJson from "../../../package.json"
-
 // Add hook/detection for users to opt out of tracking
 export const DONT_TRACK = false
 export const AMPLITUDE_API_KEY = process.env.REACT_APP_AMPLITUDE_API_KEY
@@ -17,7 +15,11 @@ export class NullAmplitudeClient implements IAmplitudeClient {
 // live tracker
 export class AmplitudeClient implements IAmplitudeClient {
   constructor(apiKey: string) {
-    amplitude.init(apiKey, { appVersion: `web-app_${packageJson.version}` })
+    amplitude.init(apiKey, {
+      appVersion: process.env.REACT_APP_VERSION
+        ? `web-app_${process.env.REACT_APP_VERSION}`
+        : "web-app",
+    })
   }
 
   trackEvent(eventLabel: string, eventOptions?: Record<string, any>): void {
