8d28ec583e285cd468b009f7b6d07ad98bedef5b
gregnuj <greg@terra.money>
2023-07-14 17:16:02 -0500

    switch to walletkit

--- src/auth/modules/select/SwitchWallet.tsx
+++ src/auth/modules/select/SwitchWallet.tsx
@@ -11,7 +11,7 @@ import useAuth from "../../hooks/useAuth"
 import is from "../../scripts/is"
 import SelectPreconfigured from "./SelectPreconfigured"
 import styles from "./SwitchWallet.module.scss"
-import { useWallet } from "@terra-money/use-wallet"
+import { useWallet } from "@terra-money/wallet-kit"
 import usePreconfigured from "auth/hooks/usePreconfigured"
 
 const SwitchWallet = () => {
