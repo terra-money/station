288a3ff4bbf8eab749d7d92765bb05f09582d42d
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-08-18 17:58:46 +0200

    Pause requests when chain is disabled (#499)
    * Pause requests when chain is disabled
    
    * fix: add loading progress and code cleanup
    
    ---------
    
    Co-authored-by: plubber <er.h.gorski@gmail.com>

--- src/txs/stake/StakeTx.tsx
+++ src/txs/stake/StakeTx.tsx
@@ -4,50 +4,44 @@ import { getAvailableStakeActions } from "data/queries/staking"
 import { useDelegations, useValidators } from "data/queries/staking"
 import { combineState } from "data/query"
 import { useBalances } from "data/queries/bank"
-import { Auto, Page, Tabs, Card } from "components/layout"
+import { Auto, Page, Tabs, Card, Flex } from "components/layout"
 import ValidatorCompact from "pages/stake/ValidatorCompact"
 import StakeForm, { StakeAction } from "./StakeForm"
 import { useNetwork } from "data/wallet"
-import { ValAddress } from "@terra-money/feather.js"
-import { useMemo } from "react"
 import styles from "./StakeTx.module.scss"
 import {
   getAvailableAllianceStakeActions,
   useAllianceDelegations,
 } from "data/queries/alliance"
 import StakingDetailsCompact from "pages/stake/StakingDetailsCompact"
+import { getChainIDFromAddress } from "utils/bech32"
+import { LoadingCircular } from "components/feedback"
 
 const StakeTx = () => {
   const { t } = useTranslation()
   const { address: destination, denom: paramDenom } = useParams() // destination validator
   const networks = useNetwork()
 
-  const network = useMemo(() => {
-    if (!destination || !ValAddress.validate(destination)) return null
-    return Object.values(networks ?? {}).find(
-      ({ prefix }) => prefix === ValAddress.getPrefix(destination)
-    )
-  }, [networks, destination])
+  const chainID = getChainIDFromAddress(destination, networks) ?? ""
+
+  const network = networks[chainID]
 
   if (!destination) throw new Error("Validator is not defined")
-  if (!network) throw new Error("Validator not found or invalid")
 
-  const denom = paramDenom?.replaceAll("=", "/") || network.baseAsset
-  const isAlliance = denom !== network.baseAsset
+  const denom = paramDenom?.replaceAll("=", "/") || network?.baseAsset
+  const isAlliance = denom !== network?.baseAsset
 
   const location = useLocation()
   const initialTab = location.state as string
 
   const { data: balances, ...balancesState } = useBalances()
-  const { data: validators, ...validatorsState } = useValidators(
-    network?.chainID
-  )
+  const { data: validators, ...validatorsState } = useValidators(chainID)
   const { data: delegations, ...delegationsState } = useDelegations(
-    network?.chainID,
+    chainID,
     isAlliance
   )
   const { data: allianceDelegations, ...allianceDelegationsState } =
-    useAllianceDelegations(network?.chainID, !isAlliance)
+    useAllianceDelegations(chainID, !isAlliance)
 
   const state = combineState(
     balancesState,
@@ -75,7 +69,12 @@ const StakeTx = () => {
   }
 
   const renderTab = (tab: StakeAction) => {
-    if (!(balances && validators)) return null
+    if (!(balances && validators))
+      return (
+        <Flex>
+          <LoadingCircular />
+        </Flex>
+      )
     if (isAlliance) {
       if (!allianceDelegations) return null
       const props = {
@@ -83,7 +82,7 @@ const StakeTx = () => {
         destination,
         balances,
         validators,
-        chainID: network?.chainID,
+        chainID,
         denom,
         details: {
           isAlliance,
@@ -98,7 +97,7 @@ const StakeTx = () => {
         destination,
         balances,
         validators,
-        chainID: network?.chainID,
+        chainID,
         denom,
         details: {
           isAlliance,
@@ -114,7 +113,7 @@ const StakeTx = () => {
       <Auto
         columns={[
           <Tabs
-            tabs={Object.values(StakeAction ?? {}).map((tab) => {
+            tabs={Object.values(StakeAction).map((tab) => {
               return {
                 key: tab,
                 tab: t(tab),
