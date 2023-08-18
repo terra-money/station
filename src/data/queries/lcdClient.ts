4c45b4908aa2de33e47e826cc9c7a140e7600d92
Michael Merz <mwmerz@protonmail.com>
2023-08-18 01:49:25 -0400

    hotfix: override terra lcds for gov endpoints.

--- src/data/queries/lcdClient.ts
+++ src/data/queries/lcdClient.ts
@@ -2,10 +2,36 @@ import { useMemo } from "react"
 import { LCDClient as InterchainLCDClient } from "@terra-money/feather.js"
 import { LCDClient } from "@terra-money/terra.js"
 import { useChainID, useNetwork } from "data/wallet"
+import { ChainID, InterchainNetwork } from "types/network"
 
 export const useInterchainLCDClient = () => {
   const network = useNetwork()
   const lcdClient = useMemo(() => new InterchainLCDClient(network), [network])
+  return lcdClient
+}
+
+export const useInterchainLCDClientGovOverride = () => {
+  const network = useNetwork()
+  const networksWithVersion = Object.fromEntries(
+    Object.entries(network as Record<ChainID, InterchainNetwork>).map(
+      ([key, value]) => {
+        if (key === "phoenix-1") {
+          return [
+            key,
+            {
+              ...value,
+              lcd: "https://terra-api.polkachu.com/",
+            },
+          ]
+        }
+        return [key, value]
+      }
+    )
+  )
+  const lcdClient = useMemo(
+    () => new InterchainLCDClient(networksWithVersion),
+    [networksWithVersion]
+  )
   return lcdClient
 }
 
