ced448f3b6ef3116b346fe4141fbd6d0c0d34537
Michael Merz <mwmerz@protonmail.com>
2023-08-24 16:32:58 -0400

    feat: send decimal places into staking components.

--- src/txs/stake/QuickStakeForm.tsx
+++ src/txs/stake/QuickStakeForm.tsx
@@ -12,7 +12,7 @@ import { Form, FormHelp, FormItem, FormWarning, Input } from "components/form"
 import { getPlaceholder, toInput } from "../utils"
 import validate from "../validate"
 import Tx from "../Tx"
-import { useNativeDenoms } from "data/token"
+import { useNativeDenoms, DEFAULT_NATIVE_DECIMALS } from "data/token"
 import {
   getQuickStakeMsgs,
   getQuickUnstakeMsgs,
@@ -97,7 +97,12 @@ const QuickStakeForm = (props: Props) => {
   const { register, trigger, watch, setValue, handleSubmit, formState } = form
   const { errors } = formState
   const { input } = watch()
-  const amount = toAmount(input)
+  const { decimals } = readNativeDenom(denom)
+
+  const amount = toAmount(
+    input,
+    decimals ? { decimals } : { decimals: DEFAULT_NATIVE_DECIMALS }
+  )
 
   const daysToUnbond = getChainUnbondTime(stakeParams?.unbonding_time)
 
@@ -108,8 +113,15 @@ const QuickStakeForm = (props: Props) => {
 
   const stakeMsgs = useMemo(() => {
     if (!address || !elegibleVals) return
-    const coin = new Coin(denom, toAmount(input || toInput(1)))
     const { decimals } = readNativeDenom(denom)
+    const coin = new Coin(
+      denom,
+      toAmount(
+        input || toInput(1),
+        decimals ? { decimals } : { decimals: DEFAULT_NATIVE_DECIMALS }
+      )
+    )
+
     return getQuickStakeMsgs(
       address,
       coin,
@@ -132,7 +144,14 @@ const QuickStakeForm = (props: Props) => {
 
   const unstakeMsgs = useMemo(() => {
     if (!address || !(isAlliance ? allianceDelegations : delegations)) return
-    const coin = new Coin(denom, toAmount(input || toInput(1)))
+    const { decimals } = readNativeDenom(denom)
+    const coin = new Coin(
+      denom,
+      toAmount(
+        input || toInput(1),
+        decimals ? { decimals } : { decimals: DEFAULT_NATIVE_DECIMALS }
+      )
+    )
     return getQuickUnstakeMsgs(
       address,
       coin,
@@ -153,6 +172,7 @@ const QuickStakeForm = (props: Props) => {
     allianceDelegations,
     allianceHubContract,
     stakeOnAllianceHub,
+    readNativeDenom,
   ])
 
   /* tx */
@@ -206,8 +226,9 @@ const QuickStakeForm = (props: Props) => {
   const feeTokenSymbol = readNativeDenom(network[chainID].baseAsset).symbol
 
   const token = action === QuickStakeAction.DELEGATE ? denom : ""
+
   const tx = {
-    decimals: readNativeDenom(token)?.decimals,
+    decimals: readNativeDenom(denom)?.decimals,
     token,
     amount,
     balance,
@@ -268,7 +289,13 @@ const QuickStakeForm = (props: Props) => {
                 <Input
                   {...register("input", {
                     valueAsNumber: true,
-                    validate: validate.input(toInput(max.amount)),
+                    validate: validate.input(
+                      toInput(
+                        max.amount,
+                        decimals ? decimals : DEFAULT_NATIVE_DECIMALS
+                      ),
+                      decimals ? decimals : DEFAULT_NATIVE_DECIMALS
+                    ),
                   })}
                   type="number"
                   token={denom}
@@ -301,7 +328,7 @@ const QuickStakeForm = (props: Props) => {
                         Station.
                       </li>
                       <li>
-                       Send {feeTokenSymbol} to {network[chainID].name} by
+                        Send {feeTokenSymbol} to {network[chainID].name} by
                         clicking 'Send' on your wallet sidebar and selecting
                         your {network[chainID].name} address from your address
                         book
