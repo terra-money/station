bfd49ae644cef52ade9751f02df89fcbf1675fa1
emidev98 <emilian@terra.money>
2023-09-13 18:42:32 +0800

    feat: nostr-tools compatibility

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,14 +1,11 @@
-import { useAuth } from "auth"
-import { Event, Kind } from "data/nostr/event"
-import { relayInit } from "data/nostr/relay"
+import { Event, Kind, relayInit } from "nostr-tools"
 import { useEffect, useState } from "react"
 
 export const Chat = () => {
-  const { getConnectedWallet } = useAuth()
   const [relay, setRelay] = useState(relayInit("ws://localhost:7447"))
   const [pubStatus, setPubStatus] = useState("")
-  const [newEvent, setNewEvent] = useState<Event<any>>()
-  const [events, setEvents] = useState<Event<any>[]>([])
+  const [newEvent, setNewEvent] = useState<Event>()
+  const [events, setEvents] = useState<Event[]>([])
 
   useEffect(() => {
     const connectRelay = async () => {
@@ -16,6 +13,7 @@ export const Chat = () => {
 
       relay.on("connect", () => {
         setRelay(relay)
+        getEvent()
       })
       relay.on("error", () => {
         console.log("failed to connect")
@@ -23,7 +21,7 @@ export const Chat = () => {
     }
 
     connectRelay()
-  })
+  }, [])
 
   const publishEvent = (event: any) => {
     console.log(event)
@@ -50,49 +48,11 @@ export const Chat = () => {
         ],
       },
     ])
-    sub.on("event", (event) => {
+    sub.on("event", (event: any) => {
       console.log(event)
       setNewEvent(event)
     })
   }
 
-  const getEvents = async () => {
-    let events = await relay.list([
-      {
-        kinds: [
-          Kind.ChannelCreation,
-          Kind.ChannelMetadata,
-          Kind.ChannelMessage,
-          Kind.ChannelHideMessage,
-          Kind.ChannelMuteUser,
-        ],
-      },
-    ])
-    console.log(events)
-    setEvents(events)
-  }
-
-  return (
-    <div>
-      {relay ? (
-        <p>Connected to {relay.url}</p>
-      ) : (
-        <p>Could not connect to relay</p>
-      )}
-      <p>Publish status: {pubStatus}</p>
-      <button onClick={() => getEvent()}>subscribe event</button>
-      {newEvent ? (
-        <p>Subscribed event content: {newEvent.content}</p>
-      ) : (
-        <p>no new event</p>
-      )}
-      <button onClick={() => getEvents()}>load feed</button>
-      {events !== null &&
-        events.map((event) => (
-          <p key={event.sig} style={{ borderStyle: "ridge", padding: 10 }}>
-            {event.content}
-          </p>
-        ))}
-    </div>
-  )
+  return <div></div>
 }
